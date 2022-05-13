295e264fade4c044030965f60b57afc80a5fc4d7
Eugenia Galkina <yavsehmilee@gmail.com>
2022-05-13 20:23:29 +0300

    palindromos 5digits

---
+++ task2/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.WriteLine("введите пятизначное число ");
+int a = int.Parse(Console.ReadLine() ?? "0");
+if (a>99999)
+Console.WriteLine("это число не пятизначное!");
+else if (a/10000 == a%10 && a/1000%10 == a/10%10)
+{
+    Console.WriteLine("это число палиндром");
+}
+else
+{
+    Console.WriteLine("это число не палиндром");
+}
