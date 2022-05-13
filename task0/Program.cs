f6fdf5aff552d2ccfb56c2f9102240f89606dcc9
Eugenia Galkina <yavsehmilee@gmail.com>
2022-05-13 18:38:25 +0300

    wrong decision

--- task0/Program.cs
+++ task0/Program.cs
@@ -1,2 +1,20 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.WriteLine("введите число ");
+int a = int.Parse(Console.ReadLine() ?? "0");
+int b = a%7;
+int c = a%23;
+if (b==0)
+{
+Console.Write("это число кратно 7!");
+}
+else
+{
+Console.Write("это число некратно 7!");
+}
+if (c==0)
+{
+Console.Write("это число кратно 23!");
+}
+else
+{
+Console.Write("это число некратно 23!");
+}
