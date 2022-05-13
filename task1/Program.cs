05f5d8ef69a579d297fd1f8aae8ba120a5910570
Eugenia Galkina <yavsehmilee@gmail.com>
2022-05-13 19:21:57 +0300

    tried to find the  decision

--- task1/Program.cs
+++ task1/Program.cs
@@ -1,2 +1,14 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.WriteLine("введите координату Х");
+Console.WriteLine("введите координату Y");
+int x = int.Parse(Console.ReadLine() ?? "0");
+int y = int.Parse(Console.ReadLine() ?? "0");
+if (x==0 || y==0)
+Console.WriteLine ("Х и У не равны нулю, введите другие координаты!");
+if (x>0 && y>0)
+Console.WriteLine ("I четверть!");
+if (x<0 && y>0)
+Console.WriteLine ("II четверть!");
+if (x<0 && y<0)
+Console.WriteLine ("III четверть!");
+if (x>0 && y<0)
+Console.WriteLine ("IV четверть!");
